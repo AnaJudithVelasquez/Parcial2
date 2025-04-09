a11b8286250b13fdabec280fd334f2d8421baee0
Ana Velasquez <anajudith1309@gmail.com>
2025-04-08 20:36:22 -0500

    Maria ya esta clase Guerrero

--- src/parcial2/Dragon.java
+++ src/parcial2/Dragon.java
@@ -1,27 +1,31 @@
 
 package parcial2;
 
-   public class Dragon extends Criatura {
+   public class Dragon extends Criatura { // se declara la clase dragon que hereda de criatura 
 
-    public Dragon(String nombre, int salud, int fuerza) {
+    public Dragon(String nombre, int salud, int fuerza) {  // se genera el metodo constructor
         super(nombre, salud, fuerza);
     }
 
     @Override
-    public void atacar(Criatura objetivo) {
-        int daño = fuerza * 2;
+    public void atacar(Criatura objetivo) {   // se reescribe el metodo atacar
+        int daño = fuerza * 2; //calcula el daño que va causar el ataque
         System.out.println(nombre + " lanza fuego a " + objetivo.getNombre() + " causando " + daño + " de daño.");
-        objetivo.defender(daño);
+        objetivo.defender(daño);//se llama al metodo defender del objetivo para pasar el daño calculado y que se reduzca su salud
     }
 
-    @Override
+  
+       @Override
     public void defender(int daño) {
-        int dañoRecibido = daño - 5;
-        dañoRecibido = Math.max(dañoRecibido, 0);
-        salud = salud-dañoRecibido;
-                System.out.println(nombre + " recibe " + dañoRecibido + " de daño y su salud restante es : " + salud);
+    int dañoRecibido = daño - 5; //
+    if (dañoRecibido < 0) {
+        dañoRecibido = 0;
     }
-}
+    salud = salud - dañoRecibido;
+    System.out.println(nombre + " recibe " + dañoRecibido +  " de daño y su salud restante es: " + salud);
+    }
+ }
+
 
 
     
--- src/parcial2/Parcial2.java
+++ src/parcial2/Parcial2.java
@@ -7,8 +7,17 @@ public class Parcial2 {
     
     public static void main(String[] args) {
     
+        Criatura dragon = new Dragon ("Power", 100, 20);
+        Criatura mago = new Mago ("Gargamel", 80, 25);
+        Criatura guerrero = new Guerrero ("Hercules", 90, 18);
         
         
+        
+    }
+    
+    public void simularBatalla(){
+    
+    
     }
     
 }
