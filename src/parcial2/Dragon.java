c71d16318657eb15da25ef4bdda41621ed1897bd
MariadelMarPerez <mariadelmarperezmontengro@gmail.com>
2025-04-09 11:42:51 -0500

    Ana realice comentarios en los codigos de la clases para explicar su proposito,solo falta comentar la clase guerrero

--- src/parcial2/Dragon.java
+++ src/parcial2/Dragon.java
@@ -3,6 +3,7 @@ package parcial2;
 public class Dragon extends Criatura { // se declara la clase dragon que hereda de criatura 
 private String escamas;
 
+// se crea el costructor para inicializar los atributos heredades y el especifo que es su forma de defensa
     public Dragon(String escamas, String nombre, int salud, int fuerza) {
         super(nombre, salud, fuerza);
         this.escamas = escamas;
@@ -16,14 +17,14 @@ private String escamas;
     }
 
     @Override
-    public void defender(int daño) {
-        int dañoRecibido = daño - 10; //
-        if (dañoRecibido < 0) {
-            dañoRecibido = 0;
+    public void defender(int daño) { //se reescribe el metodo defender 
+        int dañoRecibido = daño - 10; // indica que por su forma de defensa en el dano causado se reducira un 10 de dano
+        if (dañoRecibido < 0) { // se indica que en caso que el dano sea negativo se le asignara el valor de 0
+            dañoRecibido = 0; 
         }
-        salud = salud - dañoRecibido;
+        salud = salud - dañoRecibido; // se hace la reduccio en la salud del dano recibido
         System.out.println(nombre + " recibe " + dañoRecibido + " de dano y se defiende con su " + escamas +  " Su salud restante es: " + salud);
-    }
+    } // se indica cual es su salud restante despues de la reducciom
 }
 
 
--- src/parcial2/Mago.java
+++ src/parcial2/Mago.java
@@ -1,9 +1,10 @@
 
 package parcial2;
 
-public class Mago extends Criatura {
-    private String hechizos;
-
+public class Mago extends Criatura { // se declara  clase Mago que hereda de criatura 
+    private String hechizos; // se declara la forma de defensa del mago
+    
+    // se inicializa los atributos heredados y el especifico
     public Mago(String hechizos, String nombre, int salud, int fuerza) {
         super(nombre, salud, fuerza);
         this.hechizos = hechizos;
@@ -11,21 +12,21 @@ public class Mago extends Criatura {
 
    
     @Override
-    public void atacar(Criatura objetivo) {
-        int daño = fuerza;
+    public void atacar(Criatura objetivo) { //se rescribe el metodo atacar
+        int daño = fuerza; // se indica que el dano va a ser causado con la fuerza dada a la criatura 
         System.out.println(nombre + " lanza un hechizo a " + objetivo.getNombre() + " causando " + daño + " de dano.");
-        objetivo.defender(daño);
+        objetivo.defender(daño); // se imprime por pantalla un mensaje que indique el total de dano causado
     }
 
     @Override
-    public void defender(int daño) {
-        int dañoRecibido = daño - 10;
+    public void defender(int daño) { // se reescribe el metodo defender
+        int dañoRecibido = daño - 10; // indica que por su forma de defensa en el dano causado se reducira un 10 de dano
         if (dañoRecibido < 0) {
             dañoRecibido = 0;
         }
         salud = salud - dañoRecibido;
         System.out.println(nombre + " recibe " + dañoRecibido + " de dano  y se protege con su " + hechizos + " Su salud restante es : " + salud);
-    }
+    } // se indica por pantalla cual es su salud restantes despues de la reduccion
 }
 
 
